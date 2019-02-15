9d889dbb43dfa0e8be7d9189494881c38449344b
Ambrose Hundal <amshunda@ucsc.edu>
2019-02-15 14:01:49 -0800

    paired plates queries and more

--- src/index.js
+++ src/index.js
@@ -124,9 +124,9 @@ app.get('/platelist/:id', function (req, res){
     var plate_unique_id = req.params.id;
     console.log("plate unique ID is: " + plate_unique_id);
    
-    //var platequeryString = 'SELECT * FROM platelist WHERE id=' + var_id;
+    var pair_order = ' ORDER BY plate_pair_id ASC' 
    
-    var platelistquery = 'SELECT plate.unique_plate_id, plate.UCSC_CSC_plate_ID, plate.Cell_lines, plate.TimePoint, plate.Magnification, plate.experiment_date, plate.plate_pair_id FROM plate INNER JOIN platelist ON (platelist.unique_plate_id=plate.unique_plate_id) WHERE plate.unique_plate_id=' + plate_unique_id;
+    var platelistquery = 'SELECT plate.unique_plate_id, plate.UCSC_CSC_plate_ID, plate.Cell_lines, plate.TimePoint, plate.Magnification, plate.experiment_date, plate.plate_pair_id FROM plate INNER JOIN platelist ON (platelist.unique_plate_id=plate.unique_plate_id) WHERE plate.unique_plate_id=' + plate_unique_id + pair_order;
    
 
 
